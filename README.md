72e63279704b934e9d4682fbe9d11ad236cc31db
rdeepak2002 <rdeepak2002@gmail.com>
2020-07-19 22:59:48 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# TortHub
+## Author
+Deepak Ramalingam
+
+## Requirements for Raspberry Pi 4 (Buster)
+* python3
+* pip3 install from requirements.txt
+* the following packages must be installed on the system:
+  * sudo apt-get install libatlas-base-dev
+  * sudo apt-get install libjasper-dev
+  * sudo apt-get install libqtgui4
+  * sudo apt-get install python3-pyqt5
+  * sudo apt-get install libqt4-test
---
+++ camera_opencv.py
@@ -0,0 +1,39 @@
+import os
+import cv2
+from base_camera import BaseCamera
+
+
+class Camera(BaseCamera):
+    video_source = 0
+
+    def __init__(self):
+        if os.environ.get('OPENCV_CAMERA_SOURCE'):
+            Camera.set_video_source(int(os.environ['OPENCV_CAMERA_SOURCE']))
+        super(Camera, self).__init__()
+
+    @staticmethod
+    def set_video_source(source):
+        Camera.video_source = source
+
+    @staticmethod
+    def frames():
+        camera = cv2.VideoCapture(Camera.video_source)
+        if not camera.isOpened():
+            raise RuntimeError('Could not start camera.')
+
+        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+        while True:
+            # read current frame
+            _, img = camera.read()
+
+
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            # Detect the faces
+            faces = face_cascade.detectMultiScale(gray, 1.4, 4)
+            # Draw the rectangle around each face
+            for (x, y, w, h) in faces:
+                cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+            # encode as a jpeg image and return it
+            yield cv2.imencode('.jpg', img)[1].tobytes()
