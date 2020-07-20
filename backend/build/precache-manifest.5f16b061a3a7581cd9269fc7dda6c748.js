f0e3d7c49384c891198e1a3258e8930e511fd2bd
rdeepak2002 <rdeepak2002@gmail.com>
2020-07-19 23:54:29 -0700

    added build folder to back

---
+++ backend/build/precache-manifest.5f16b061a3a7581cd9269fc7dda6c748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "052d5b890312d61e2a8ffeda91225a9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "171b671dcbca072a15f6",
+    "url": "/static/css/main.88824ec2.chunk.css"
+  },
+  {
+    "revision": "eb88ab92a449247c82c7",
+    "url": "/static/js/2.f20be21e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f20be21e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "171b671dcbca072a15f6",
+    "url": "/static/js/main.78fb94cd.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
