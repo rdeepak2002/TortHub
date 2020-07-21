0e8db7f0c93b72d7dbde187d1607ef92e9e8a3cd
rdeepak2002 <rdeepak2002@gmail.com>
2020-07-20 20:43:37 -0700

    added time to video feed

--- backend/camera_opencv.py
+++ backend/camera_opencv.py
@@ -1,6 +1,8 @@
 import os
 import cv2
+import numpy
 from base_camera import BaseCamera
+from datetime import datetime
 
 class Camera(BaseCamera):
     video_source = 0
@@ -20,18 +22,45 @@ class Camera(BaseCamera):
         if not camera.isOpened():
             raise RuntimeError('Could not start camera.')
 
-        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        # face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
         while True:
             # read current frame
             _, img = camera.read()
 
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            # uncomment for face detection
+            # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             # Detect the faces
-            faces = face_cascade.detectMultiScale(gray, 1.4, 4)
+            # faces = face_cascade.detectMultiScale(gray, 1.4, 4)
             # Draw the rectangle around each face
-            for (x, y, w, h) in faces:
-                cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            # for (x, y, w, h) in faces:
+            #     cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+            text = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+            font                    = cv2.FONT_HERSHEY_SIMPLEX
+            fontScale               = 2
+            whiteColor              = (255,255,255)
+            blackColor              = (0, 0, 0)
+            thickLine               = 6
+            thinLine                = 4
+
+            size = cv2.getTextSize(text, font, 2, 4)[0]
+            cv2.putText(img, text, (10,size[1]+10), font, fontScale, blackColor, thickLine)
+            cv2.putText(img, text, (10,size[1]+10), font, fontScale, whiteColor, thinLine)
+
+            # cv2.putText(img, datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
+            #     bottomLeftCornerOfText,
+            #     font,
+            #     fontScale*1.1,
+            #     blackColor,
+            #     lineType)
+            #
+            # cv2.putText(img, datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
+            #     bottomLeftCornerOfText,
+            #     font,
+            #     fontScale,
+            #     whiteColor,
+            #     lineType)
 
             # encode as a jpeg image and return it
             yield cv2.imencode('.jpg', img)[1].tobytes()
