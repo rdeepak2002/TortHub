bcbb0adf83e927c6bb3d63cce1e5c48762c6a26c
rdeepak2002 <rdeepak2002@gmail.com>
2020-07-26 18:39:23 -0700

    fixed router navbar issues

---
+++ backend/build/precache-manifest.d6ef0e2d5af3c193971c88c93d77c0c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9df381877f8087bbe915eb15b5e48303",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58aa258799a2c24a1cf9",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4451944f5b155b9b03d5",
+    "url": "/static/css/main.9b040553.chunk.css"
+  },
+  {
+    "revision": "58aa258799a2c24a1cf9",
+    "url": "/static/js/2.c1bf2e23.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c1bf2e23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4451944f5b155b9b03d5",
+    "url": "/static/js/main.a3113b79.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
