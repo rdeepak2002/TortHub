a4f78818a987be363289e19aeeee0ff3ab841e43
rdeepak2002 <rdeepak2002@gmail.com>
2020-07-27 21:05:50 -0700

    added mongo connection

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,9 @@
+# imports
 from importlib import import_module
 from flask import Flask, Response, jsonify, send_from_directory, send_file
 from camera_opencv import Camera
+from threading import Thread
+from tasks import update_temp_humid_data
 import os
 try:
     import Adafruit_DHT
@@ -11,14 +14,21 @@ try:
 except:
     print("Error: Unable to import RFDevice from rpi_rf!")
 
+# flask app
 app = Flask(__name__, static_folder='build')
 
+# background thread for updating temperature data
+thread = Thread(target=update_temp_humid_data)
+thread.daemon = True
+thread.start()
 
+# generator for video stream
 def gen(camera):
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+# default path for react spa to work properly
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve(path):
@@ -27,21 +37,17 @@ def serve(path):
     else:
         return send_from_directory(app.static_folder, 'index.html')
 
+# route to return video stream
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+# get request for getting sensor data
 @app.route('/sensor_stats', methods=['GET'])
 def sensor_stats():
-    try:
-        humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
-        return jsonify({
-            'temperature':str(temperature),
-            'humidity':str(humidity)
-        })
-    except:
-        return Response("Error with reading from sensor.")
+    return Response("Return mongo data here")
 
+# get request to turn off light
 @app.route('/turnoff_light', methods=['GET'])
 def turnoff_light():
     try:
@@ -54,6 +60,7 @@ def turnoff_light():
     except:
         return Response("Error with turning off light.")
 
+# get request to turn on light
 @app.route('/turnon_light', methods=['GET'])
 def turnon_light():
     try:
@@ -66,10 +73,12 @@ def turnon_light():
     except:
         return Response("Error with turning on light.")
 
+# return file for SSL validation
 @app.route('/.well-known/pki-validation/<challenge>')
 def verify_ssl(challenge):
     filename = app.root_path + '/.well-known/' + challenge
     return send_file(filename)
 
+# main method
 if __name__ == '__main__':
     app.run(host='0.0.0.0', threaded=True, debug=True)
---
+++ backend/tasks.py
@@ -0,0 +1,34 @@
+# imports
+import time
+import datetime
+import pymongo
+try:
+    import Adafruit_DHT
+except:
+    print("Error: Unable to import Adafruit_DHT!")
+
+# connection to mongo db
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+database = client["torthubdb"]
+tempCol = database["temperature"]
+humidCol = database["humidity"]
+
+# thread to get temp and humidity in background
+def update_temp_humid_data():
+    while True:
+        try:
+            humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
+            updateTempHumidInDb(temperature, humidity)
+        except:
+            print("error reading from sensor")
+
+        time.sleep(10)
+
+# method to update data in mongo
+def updateTempHumidInDb(temperature, humidity):
+    current_time = datetime.datetime.now()
+    tempData = { "time": current_time, "temperature": temperature }
+    humidData = { "time": current_time, "humidity": humidity }
+
+    tempCol.insert_one(tempData)
+    humidCol.insert_one(humidData)
