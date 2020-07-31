da6fe51285baebaf0adfebe659986b4805fade17
rdeepak2002 <rdeepak2002@gmail.com>
2020-07-30 22:44:26 -0700

    added alert on update

---
+++ backend/build/precache-manifest.7c6502733b8e221cf6b815b99bbcaffc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "206743254baa263d4baa66179e9e7ef9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c8c7bb477247fc5b243",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "21c9a256baa5e41c3823",
+    "url": "/static/css/main.8ab79d7b.chunk.css"
+  },
+  {
+    "revision": "6c8c7bb477247fc5b243",
+    "url": "/static/js/2.6a9602d6.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/static/js/2.6a9602d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21c9a256baa5e41c3823",
+    "url": "/static/js/main.1c0befe4.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
